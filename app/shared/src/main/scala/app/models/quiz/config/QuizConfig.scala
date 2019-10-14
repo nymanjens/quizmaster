e59a1746a08060fcc804f255c5205f3410ee5421
Jens Nyman <nymanjens.nj@gmail.com>
2019-10-14 21:44:03 +0200

    Add QuizConfig

---
+++ app/shared/src/main/scala/app/models/quiz/config/QuizConfig.scala
@@ -0,0 +1,42 @@
+package app.models.quiz.config
+
+import app.models.quiz.config.QuizConfig.Round
+
+import scala.concurrent.duration._
+import scala.concurrent.duration.Duration
+
+case class QuizConfig(
+    rounds: Seq[Round],
+)
+
+object QuizConfig {
+
+  case class Round(
+      name: String,
+      questions: Seq[Question],
+  )
+
+  sealed trait Question
+
+  object Question {
+    case class Single(
+        question: String,
+        answer: String,
+        choices: Option[Seq[String]],
+        pointsToGain: Int,
+        maxTime: Option[Duration],
+        onlyFirstGainsPoints: Boolean,
+    ) extends Question
+
+    case class Double(
+        verbalQuestion: String,
+        verbalAnswer: String,
+        textualQuestion: String,
+        textualAnswer: String,
+        textualChoices: Option[Seq[String]],
+        pointsToGain: Int,
+    ) extends Question {
+      def maxTime: Duration = 3.seconds
+    }
+  }
+}
