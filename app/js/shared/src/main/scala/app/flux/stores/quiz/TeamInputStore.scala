ef5b255e72df5d2d58c0c4b193c71c39c44e52be
Jens Nyman <nymanjens.nj@gmail.com>
2019-10-18 23:29:21 +0200

    Add TeamInputStore

---
+++ app/js/shared/src/main/scala/app/flux/stores/quiz/TeamInputStore.scala
@@ -0,0 +1,50 @@
+package app.flux.stores.quiz
+
+import app.flux.stores.quiz.GamepadStore.GamepadState
+import app.flux.stores.quiz.TeamInputStore.State
+import app.models.quiz.config.QuizConfig
+import app.models.user.User
+import hydro.common.time.Clock
+import hydro.flux.action.Dispatcher
+import hydro.flux.stores.StateStore
+import hydro.models.access.JsEntityAccess
+
+import scala.concurrent.Future
+import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
+
+final class TeamInputStore(
+    implicit entityAccess: JsEntityAccess,
+    user: User,
+    dispatcher: Dispatcher,
+    clock: Clock,
+    quizConfig: QuizConfig,
+    teamsAndQuizStateStore: TeamsAndQuizStateStore,
+    gamepadStore: GamepadStore,
+) extends StateStore[State] {
+  private var _state: State = State.nullInstance
+  gamepadStore.register(GamepadStoreListener)
+
+  override def state: State = _state
+
+  private def setState(newState: State): Unit = {
+    if (newState != _state) {
+      _state = newState
+      Future(invokeStateUpdateListeners())
+    }
+  }
+
+  private object GamepadStoreListener extends StateStore.Listener {
+    override def onStateUpdate(): Unit = {
+      val teams = teamsAndQuizStateStore.stateOrEmpty.teams
+      setState(State(teamIdToGamepadState = (teams.map(_.id) zip gamepadStore.state.gamepads).toMap))
+      // TODO: Update QuizState if button pressed on right moment
+    }
+  }
+}
+
+object TeamInputStore {
+  case class State(teamIdToGamepadState: Map[Long, GamepadState] = Map())
+  object State {
+    def nullInstance = State()
+  }
+}
