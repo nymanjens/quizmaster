aef06b9cb06c7e915f57a9fa63736b893abfd0d4
Jens Nyman <nymanjens.nj@gmail.com>
2019-11-01 11:12:24 +0100

    Allow submissions when paused

--- app/shared/src/main/scala/app/models/quiz/QuizState.scala
+++ app/shared/src/main/scala/app/models/quiz/QuizState.scala
@@ -69,7 +69,9 @@ case class QuizState(
         val submissionAreOpen = question.submissionAreOpen(questionProgressIndex)
         val submissionIsHinderedByTimer =
           if (question.shouldShowTimer(questionProgressIndex))
-            !timerState.timerRunning || timerState.hasFinished(question.maybeMaxTime.get)
+            // !timerState.timerRunning || // Note: Allowing submissions even when paused to allow submissions while
+            // another team is answering
+            timerState.hasFinished(question.maybeMaxTime.get)
           else false
 
         submissionAreOpen && !submissionIsHinderedByTimer
