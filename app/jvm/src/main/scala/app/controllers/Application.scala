273933edcd0c6462877f09ead68599f89e52aa75
Jens Nyman <nymanjens.nj@gmail.com>
2019-11-01 14:26:59 +0100

    Add logging when round changes

--- app/jvm/src/main/scala/app/controllers/Application.scala
+++ app/jvm/src/main/scala/app/controllers/Application.scala
@@ -1,19 +1,28 @@
 package app.controllers
 
+import org.reactivestreams.Subscriber
 import java.nio.file.Path
 import java.nio.file.Paths
+import java.time.format.DateTimeFormatter
+import java.time.LocalDateTime
 
+import app.api.ScalaJsApi.HydroPushSocketPacket.EntityModificationsWithToken
 import app.api.ScalaJsApiServerFactory
 import app.models.access.JvmEntityAccess
+import app.models.quiz.QuizState
 import com.google.inject.Inject
 import hydro.common.time.Clock
 import hydro.common.ResourceFiles
+import hydro.models.modification.EntityModification
+import javax.inject.Singleton
+import org.reactivestreams.Subscription
 import play.api.i18n.I18nSupport
 import play.api.i18n.MessagesApi
 import play.api.mvc._
 
 import scala.concurrent.ExecutionContext
 
+@Singleton
 final class Application @Inject()(
     implicit override val messagesApi: MessagesApi,
     playConfiguration: play.api.Configuration,
@@ -26,6 +35,32 @@ final class Application @Inject()(
     externalAssetsController: controllers.ExternalAssets,
 ) extends AbstractController(components)
     with I18nSupport {
+
+  // Print round timings
+  entityAccess.entityModificationPublisher.subscribe(new Subscriber[EntityModificationsWithToken]() {
+    var lastSeenRoundIndex = -1
+
+    override def onSubscribe(s: Subscription): Unit = {}
+    override def onNext(t: EntityModificationsWithToken): Unit = {
+      t.modifications.collect {
+        case EntityModification.Add(quizState: QuizState) =>
+          println(s"  >>>> [$currentTimeString] Started round ${quizState.roundIndex + 1}")
+          lastSeenRoundIndex = quizState.roundIndex
+          lastSeenRoundIndex = quizState.roundIndex
+        case EntityModification.Update(quizState: QuizState) =>
+          if (quizState.roundIndex != lastSeenRoundIndex) {
+            println(
+              s"  >>>> [$currentTimeString] Changed round from ${lastSeenRoundIndex + 1} to ${quizState.roundIndex + 1}")
+            lastSeenRoundIndex = quizState.roundIndex
+          }
+      }
+    }
+    override def onError(t: Throwable): Unit = {}
+    override def onComplete(): Unit = {}
+
+    private def currentTimeString: String =
+      DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now)
+  })
 
   def quizImage(file: String): Action[AnyContent] = {
     externalAssetsController.at(rootPath = configPath.resolve("images").toString, file)
