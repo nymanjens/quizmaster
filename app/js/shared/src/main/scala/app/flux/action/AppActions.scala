7dae290bdaf3346df979a18547fc5dcb8bc4a927
Jens Nyman <nymanjens.nj@gmail.com>
2020-05-03 19:05:57 +0200

    Add AddSubmission action

--- app/js/shared/src/main/scala/app/flux/action/AppActions.scala
+++ app/js/shared/src/main/scala/app/flux/action/AppActions.scala
@@ -1,6 +1,10 @@
 package app.flux.action
 
+import app.models.quiz.QuizState.Submission.SubmissionValue
+import hydro.flux.action.Action
+
 object AppActions {
 
   // **************** Quiz-related actions **************** //
+  case class AddSubmission(teamId: Long, submissionValue: SubmissionValue) extends Action
 }
