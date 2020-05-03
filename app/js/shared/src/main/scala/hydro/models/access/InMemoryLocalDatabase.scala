b16fa61f3573e49c6364bc7ad952a89b53226669
Jens Nyman <nymanjens.nj@gmail.com>
2020-05-03 15:45:47 +0200

    Partially implement InMemoryLocalDatabase

--- app/js/shared/src/main/scala/hydro/models/access/InMemoryLocalDatabase.scala
+++ app/js/shared/src/main/scala/hydro/models/access/InMemoryLocalDatabase.scala
@@ -5,21 +5,30 @@ import hydro.models.modification.EntityType
 import hydro.models.Entity
 
 import scala.collection.immutable.Seq
+import scala.concurrent.Future
 
 final class InMemoryLocalDatabase extends LocalDatabase {
 
   private val inMemoryEntityDatabase: InMemoryEntityDatabase = new InMemoryEntityDatabase()
 
-  def queryExecutor[E <: Entity: EntityType]() = ???
-  def pendingModifications() = ???
-  def getSingletonValue[V](key: SingletonKey[V]) = ???
-  def isEmpty = ???
-  def applyModifications(modifications: Seq[EntityModification]) = ???
-  def addAll[E <: Entity: EntityType](entities: Seq[E]) = ???
-  def addPendingModifications(modifications: Seq[EntityModification]) = ???
-  def removePendingModifications(modifications: Seq[EntityModification]) = ???
-  def setSingletonValue[V](key: SingletonKey[V], value: V) = ???
-  def save() = ???
-  def resetAndInitialize() = ???
+  // **************** Getters ****************//
+  override def queryExecutor[E <: Entity: EntityType](): DbQueryExecutor.Async[E] = {
+    inMemoryEntityDatabase.queryExecutor[E].asAsync
+  }
+  override def pendingModifications(): Future[Seq[EntityModification]] = ???
+  override def getSingletonValue[V](key: SingletonKey[V]): Future[Option[V]] = ???
+  override def isEmpty: Future[Boolean] = ???
 
+  // **************** Setters ****************//
+  override def applyModifications(modifications: Seq[EntityModification]): Future[Unit] = ???
+  override def addAll[E <: Entity: EntityType](entities: Seq[E]): Future[Unit] = ???
+
+  override def addPendingModifications(modifications: Seq[EntityModification]): Future[Unit] = ???
+  override def removePendingModifications(modifications: Seq[EntityModification]): Future[Unit] = ???
+
+  override def setSingletonValue[V](key: SingletonKey[V], value: V): Future[Unit] = ???
+
+  override def save(): Future[Unit] = ???
+
+  override def resetAndInitialize(): Future[Unit] = ???
 }
