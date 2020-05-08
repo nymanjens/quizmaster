876db9c5b7c54c3ad5e6dc2eb79b6258bd1140e5
Jens Nyman <nymanjens.nj@gmail.com>
2020-05-08 15:28:05 +0200

    Add SubmissionEntity

---
+++ app/shared/src/main/scala/app/models/quiz/SubmissionEntity.scala
@@ -0,0 +1,24 @@
+package app.models.quiz
+
+import java.time.Instant
+
+import app.models.quiz.QuizState.Submission.SubmissionValue
+import hydro.models.Entity
+import hydro.models.UpdatableEntity
+import hydro.models.UpdatableEntity.LastUpdateTime
+
+case class SubmissionEntity(
+    teamId: Long,
+    roundIndex: Int,
+    questionIndex: Int,
+    createTime: Instant,
+    value: SubmissionValue,
+    // If none, there is no information available to make an estimation of correctness
+    isCorrectAnswer: Option[Boolean],
+    override val idOption: Option[Long] = None,
+    override val lastUpdateTime: LastUpdateTime = LastUpdateTime.neverUpdated,
+) extends UpdatableEntity {
+
+  override def withId(id: Long) = copy(idOption = Some(id))
+  override def withLastUpdateTime(time: LastUpdateTime): Entity = copy(lastUpdateTime = time)
+}
