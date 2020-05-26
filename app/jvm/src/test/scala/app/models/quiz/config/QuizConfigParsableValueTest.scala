164be9d23eab2ab3266cd46a241ef130952f2368
Jens Nyman <nymanjens.nj@gmail.com>
2020-05-26 14:14:13 +0200

    Fix QuizConfigParsableValueTest

--- app/jvm/src/test/scala/app/models/quiz/config/QuizConfigParsableValueTest.scala
+++ app/jvm/src/test/scala/app/models/quiz/config/QuizConfigParsableValueTest.scala
@@ -1,5 +1,11 @@
 package app.models.quiz.config;
 
+import java.time.Duration
+
+import app.models.quiz.config.QuizConfig.Image
+import app.models.quiz.config.QuizConfig.Question
+import app.models.quiz.config.QuizConfig.Round
+
 import scala.collection.immutable.Seq
 import app.models.quiz.config.ValidatingYamlParser.ParseResult
 import org.junit.runner._
@@ -67,9 +73,104 @@ class QuizConfigParsableValueTest extends Specification {
         title = Some("Demo quiz"),
         author = Some("Jens Nyman"),
         masterSecret = "quiz",
-        rounds = Seq(),
-      )
-    )
+        rounds = Seq(
+          Round(
+            name = "Geography",
+            questions = Seq(
+              Question.Single(
+                question = "What is the capital of France?",
+                questionDetail = None,
+                choices = Some(Seq("Paris", "London", "Brussels", "Berlin")),
+                answer = "Paris",
+                answerDetail =
+                  Some("Image released under Creative Commons by Destination2 (www.destination2.co.uk)"),
+                answerImage = Some(Image("geography/france-answer.png", "large")),
+                masterNotes = None,
+                image = Some(Image("geography/france.png", "small")),
+                audioSrc = None,
+                pointsToGain = 2,
+                pointsToGainOnFirstAnswer = 2,
+                pointsToGainOnWrongAnswer = 0,
+                maxTime = Duration.ofSeconds(8),
+                onlyFirstGainsPoints = true,
+                showSingleAnswerButtonToTeams = false,
+              ),
+              Question.Single(
+                question = "What is the capital of Belgium?",
+                questionDetail = None,
+                choices = Some(Seq("Paris", "London", "Brussels", "Berlin")),
+                answer = "Brussels",
+                answerDetail = None,
+                answerImage = None,
+                masterNotes = None,
+                image = None,
+                audioSrc = None,
+                pointsToGain = 1,
+                pointsToGainOnFirstAnswer = 1,
+                pointsToGainOnWrongAnswer = 0,
+                maxTime = Duration.ofSeconds(60),
+                onlyFirstGainsPoints = false,
+                showSingleAnswerButtonToTeams = false,
+              ),
+              Question.Single(
+                question = "Who was the country Columbia named after?",
+                questionDetail = None,
+                choices = None,
+                answer = "Christoffer Columbus",
+                answerDetail = None,
+                answerImage = None,
+                masterNotes = None,
+                image = None,
+                audioSrc = None,
+                pointsToGain = 1,
+                pointsToGainOnFirstAnswer = 1,
+                pointsToGainOnWrongAnswer = 0,
+                maxTime = Duration.ofSeconds(8),
+                onlyFirstGainsPoints = false,
+                showSingleAnswerButtonToTeams = false,
+              )
+            ),
+            expectedTime = None
+          ),
+          Round(
+            name = "Music round",
+            questions = Seq(
+              Question.Single(
+                question = "After which season is this track named?",
+                questionDetail = Some("(Royalty Free Music from Bensound)"),
+                choices = None,
+                answer = "Summer",
+                answerDetail = Some("(By Bensound)"),
+                answerImage = None,
+                masterNotes = None,
+                image = None,
+                audioSrc = Some("music_round/bensound-summer.mp3"),
+                pointsToGain = 1,
+                pointsToGainOnFirstAnswer = 1,
+                pointsToGainOnWrongAnswer = 0,
+                maxTime = Duration.ofSeconds(15),
+                onlyFirstGainsPoints = false,
+                showSingleAnswerButtonToTeams = false,
+              ),
+            ),
+            expectedTime = None
+          ),
+          Round(
+            name = "Double questions round",
+            questions = Seq(
+              Question.Double(
+                verbalQuestion = "How many sides does a rectangle have?",
+                verbalAnswer = "4",
+                textualQuestion = "How many sides does a triangle have?",
+                textualAnswer = "3",
+                textualChoices = Seq("3", "4", "5", "6"),
+                pointsToGain = 2,
+              ),
+            ),
+            expectedTime = None,
+          )
+        ),
+      ))
   }
 
   "parse minimal file" in {
