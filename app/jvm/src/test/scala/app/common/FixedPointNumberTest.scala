19dcce44bf386632e200ccf2d74f484630070a5d
Jens Nyman <nymanjens.nj@gmail.com>
2020-07-07 23:53:41 +0200

    Use Numeric to implement FixedPointNumber's aritmetic methods

--- app/jvm/src/test/scala/app/common/FixedPointNumberTest.scala
+++ app/jvm/src/test/scala/app/common/FixedPointNumberTest.scala
@@ -35,4 +35,33 @@ class FixedPointNumberTest extends Specification {
     FixedPointNumber(2) < FixedPointNumber(-1.1) mustEqual false
     FixedPointNumber(2) > FixedPointNumber(-1.1) mustEqual true
   }
+
+  "==  int" in {
+    FixedPointNumber(1.0) == 1 mustEqual true
+    FixedPointNumber(1.1) == 1 mustEqual false
+    FixedPointNumber(0) == 0 mustEqual true
+
+    FixedPointNumber(0) != 0 mustEqual false
+    FixedPointNumber(0) != 1 mustEqual true
+  }
+
+  "sum" in {
+    Seq(FixedPointNumber(1), FixedPointNumber(1.2)).sum mustEqual FixedPointNumber(2.2)
+  }
+
+  "unapply" in {
+    (
+      FixedPointNumber(1) match {
+        case FixedPointNumber(1) => true
+        case _                   => false
+      }
+    ) mustEqual true
+
+    (
+      FixedPointNumber(1.1) match {
+        case FixedPointNumber(1) => false
+        case _                   => true
+      }
+    ) mustEqual true
+  }
 }
