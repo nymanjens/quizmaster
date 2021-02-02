25afa80d2e105be1691d0937857a804765bfa1dd
Jens Nyman <nymanjens.nj@gmail.com>
2021-02-02 09:59:29 +0100

    Add TimerAlmostRunningOut as sound effect

--- app/js/shared/src/main/scala/app/flux/controllers/SoundEffectController.scala
+++ app/js/shared/src/main/scala/app/flux/controllers/SoundEffectController.scala
@@ -41,6 +41,15 @@ final class SoundEffectController(implicit
   def playTimerRunsOut(): Unit = playSoundEffect(SoundEffect.TimerRunsOut)
   def playScoreIncreased(): Unit = playSoundEffect(SoundEffect.ScoreIncreased)
 
+  def timerAlmostRunningOutDetails: SoundDetails = SoundDetails(
+    filepath = SoundEffect.TimerAlmostRunningOut.filepath,
+    duration = 12.seconds + 720.milliseconds,
+  )
+
+  // **************** Public types ****************//
+  case class SoundDetails(filepath: String, duration: Duration)
+
+
   // **************** Private helper methods ****************//
   private def dispatcherListener: PartialFunction[Action, Unit] = {
     case StandardActions.SetPageLoadingState( /* isLoading = */ _, currentPage) =>
@@ -92,12 +101,20 @@ final class SoundEffectController(implicit
   private sealed abstract class SoundEffect(val filepath: String)
   private object SoundEffect {
     def all: Seq[SoundEffect] =
-      Seq(NewSubmission, CorrectSubmission, IncorrectSubmission, ScoreIncreased, TimerRunsOut)
+      Seq(
+        NewSubmission,
+        CorrectSubmission,
+        IncorrectSubmission,
+        ScoreIncreased,
+        TimerRunsOut,
+        TimerAlmostRunningOut,
+      )
 
     case object NewSubmission extends SoundEffect("/assets/soundeffects/new_submission.mp3")
     case object CorrectSubmission extends SoundEffect("/assets/soundeffects/correct_submission.mp3")
     case object IncorrectSubmission extends SoundEffect("/assets/soundeffects/incorrect_submission.mp3")
     case object ScoreIncreased extends SoundEffect("/assets/soundeffects/score_increased.mp3")
     case object TimerRunsOut extends SoundEffect("/assets/soundeffects/timer_runs_out.mp3")
+    case object TimerAlmostRunningOut extends SoundEffect("/assets/soundeffects/timer_almost_running_out.mp3")
   }
 }
